7d27e430e53ef6a5d1e064e981e03dbe8af10f7b
BimaSurya00 <105061172+BimaSurya00@users.noreply.github.com>
2022-07-13 00:54:53 +0700

    Add files via upload

---
+++ rekamdatawajah.py
@@ -0,0 +1,46 @@
+import cv2, os
+wajahDir = 'dataWajah'
+
+from numpy import eye
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)
+cam.set(4, 480)
+
+faceDetector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eyeDetector = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+faceID = input('Masukkan Face ID yang akan direkam datanya [kemudian tekan EN2TER] : ')
+print ('Tatap wajah ke depan anda ke dalam webcam. Tunggu proses pengambilan data wajah selesai')
+ambilData = 0
+
+while True:
+    retV, frame = cam.read()
+    abuAbu = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceDetector.detectMultiScale(abuAbu, 1.3, 5)
+    for (x, y, w, h) in faces:
+        frame = cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
+        namaFile = 'wajah.' + str(faceID)+ '.' +str(ambilData) + '.jpg'
+
+        cv2.imwrite(wajahDir+ '/' +namaFile, frame )
+        ambilData += 1
+
+        roiAbuAbu = abuAbu [ y:y+h, x:x+w ]
+        roiWarna = frame [y:y+h, x:x+w ]
+        eyes = eyeDetector.detectMultiScale(roiAbuAbu) 
+        for (xe, ye, we, he) in eyes:
+            cv2.rectangle(roiWarna, (xe, ye), (xe+we, ye+he), (0,0,255), 1 )
+
+
+    cv2.imshow('WebCamku',frame)
+    #cv2.imshow('WebCamku - Grey',abuAbu)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27 or k == ord('q'):
+        break
+
+    elif ambilData > 30:
+        break
+
+print('Pengambilan data selesai')
+
+cam.release()
+cv2.destroyAllWindows()
